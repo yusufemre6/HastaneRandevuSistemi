9ad4c4fed9b6cc594c181187e2710ca5cb7a131b
burak akbulut <92029270+akbulutburak@users.noreply.github.com>
2023-12-01 17:49:56 +0300

    Burak indexi geri getirdi test amaçlı

--- HastaneRandevuSistemi/Controllers/HomeController.cs
+++ HastaneRandevuSistemi/Controllers/HomeController.cs
@@ -13,6 +13,10 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    public IActionResult Index()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
